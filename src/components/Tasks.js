936789195dded68d7b2bf42413b72a520e1212e6
danju4rizzl <danjumashiwaju@gmail.com>
2021-01-22 21:24:20 +0200

    ✨feature: init commit

---
+++ src/components/Tasks.js
@@ -0,0 +1,17 @@
+import Task from './Task';
+const Tasks = ({ tasks, onDelete, onToggle }) => {
+  return (
+    <>
+      {tasks.map((item) => (
+        <Task
+          key={item.id}
+          task={item}
+          onDelete={onDelete}
+          onToggle={onToggle}
+        />
+      ))}
+    </>
+  );
+};
+
+export default Tasks;
